39eb37e0cdefdcbca714ec0b185bc9ecf84f7602
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-12-15 18:57:45 +0400

    Initial Commit

---
+++ CryptoInPocket/CryptoInPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoInPocketApp.swift
+//  CryptoInPocket
+//
+//  Created by Nadzeya Shpakouskaya on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoInPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
